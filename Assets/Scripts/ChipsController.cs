e1d0d96437b84e3563d7bc17d9c578b2005e9605
Andrey <andrey@entailegames.ru>
2020-02-15 14:59:19 +0200

    Create ChipsController.cs

---
+++ Assets/Scripts/ChipsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChipsController
+{
+
+    public class ChipsController : MonoBehaviour
+    {
+        private Mesh chip;
+        private void Start()
+        {
+            LoadAllResources();
+        }
+        private void LoadAllResources()
+        {
+            chip = Resources.Load<Mesh>("Models/Chip");
+            //Debug.Log(chip.ToString()); 
+        }
+        private void GenerateStack(int CountChip) { }
+        private void SplitStack(int CountChip) { }
+    }
+}
